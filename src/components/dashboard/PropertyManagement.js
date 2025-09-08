05b5b0531af9003744f25627e8f5e4b93006f72a
Matt LaBreche <support@acrnrutherford.org>
2025-09-08 04:03:28 -0400

    fix wrong call for supabase in propertymanagement.js

--- src/components/dashboard/PropertyManagement.js
+++ src/components/dashboard/PropertyManagement.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../../context/AuthContext'
-import { supabase } from '../lib/supabase';
+import { supabase } from '../../utils/supabase';
 
 const PropertyManagement = () => {
   const { user } = useAuth();
