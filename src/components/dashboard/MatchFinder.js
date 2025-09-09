a10647e2ab596f31b03a5f4c19caf69d449292da
Matt LaBreche <support@acrnrutherford.org>
2025-09-09 19:03:16 -0400

    fixed supabase hook import on matchfinder

--- src/components/dashboard/MatchFinder.js
+++ src/components/dashboard/MatchFinder.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../../context/AuthContext';
-import { useMatchingProfile } from '../hooks/useSupabase';
+import { useMatchingProfile } from '../../hooks/useSupabase';
 import { calculateDetailedCompatibility } from '../utils/matching/algorithm';
 import { generateDetailedFlags } from '../utils/matching/compatibility';
 import '../../styles/global.css';
