e8952a9d3520ad176ddb22d174c54c44b830ca6f
Matt LaBreche <support@acrnrutherford.org>
2025-09-09 18:52:43 -0400

    updated AuthContext import for MatchFinder.js

--- src/components/dashboard/MatchFinder.js
+++ src/components/dashboard/MatchFinder.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 import { useMatchingProfile } from '../hooks/useSupabase';
 import { calculateDetailedCompatibility } from '../utils/matching/algorithm';
 import { generateDetailedFlags } from '../utils/matching/compatibility';
