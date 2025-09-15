56005447051514b19bf9ad74e27067148ef334ff
Matt LaBreche <support@acrnrutherford.org>
2025-09-15 12:50:23 -0400

    fixing imports

--- src/components/auth/RegisterForm.js
+++ src/components/auth/RegisterForm.js
@@ -1,7 +1,7 @@
 // src/components/auth/RegisterForms.js
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/useAuth';
 import LoadingSpinner from '../ui/LoadingSpinner';
 import '../../styles/global.css';
 
